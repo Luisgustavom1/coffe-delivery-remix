e41f30debcf42f60885a73dff8bf0c291e754f1d
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-09-08 17:32:18 -0300

    Config remix app and tailwind css

---
+++ app/root.tsx
@@ -0,0 +1,43 @@
+import type { MetaFunction, LinksFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import styles from "@/styles/global.css";
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  { rel: "preconnect", href: "https://fonts.gstatic.com" },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Baloo+2:wght@700;800&family=Roboto:wght@400;700&display=swap",
+  },
+];
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  content: ["./app/**/*.{ts,tsx,jsx,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'yellow-light': '#F1E9C9',
+        'yellow': '#DBAC2C',
+        'yellow-dark': '#C47F17',
+        'purple-light': '#EBE5F9',
+        'purple': '#8047f8',
+        'purple-dark': '#4B2995',
+        'base-title': '#272221',
+        'base-subtitle': '#403937',
+        'base-text': '#574F4D',
+        'base-label': '#8D8686',
+        'base-hover': '#D7D5D5',
+        'base-button': '#E6E5E5',
+        'base-input': '#EDEDED',
+        'base-card': '#F3F2F2',
+        'background': '#FAFAFA',
+      },
+      screens: {
+        lg: '820px'
+      }
+    },
+    fontFamily: {
+      header: ["'Baloo 2'", 'cursive'],
+      text: ["'Roboto'", 'sans-serif'],
+    }
+  }
+};
