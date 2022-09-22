eab76ddc5bc29815a6a6216ba758f4cac135d54d
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-09-21 23:28:36 -0300

    Fix cors and correct localhost url

--- api/main.go
+++ api/main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/rs/cors"
 )
 
 func main() {
@@ -18,11 +19,19 @@ func main() {
 	}
 
 	r := chi.NewRouter()
-	r.Post("/", handlers.Create)
-	r.Put("/{id}", handlers.Update)
-	r.Delete("/{id}", handlers.Delete)
-	r.Get("/", handlers.List)
-	r.Get("/{id}", handlers.Get)
+
+	c := cors.New(cors.Options{
+		AllowCredentials: true,
+	})
+
+	r.Use(c.Handler)
+
+	r.Post("/coffes", handlers.Create)
+	r.Put("/coffes/{id}", handlers.Update)
+	r.Delete("/coffes/{id}", handlers.Delete)
+	r.Get("/coffes", handlers.List)
+	r.Get("/coffes/{id}", handlers.Get)
+	r.Get("/cart", handlers.List)
 
 	http.ListenAndServe(fmt.Sprintf(":%s", configs.GetApiServerPort()), r)
 }
