aa124cbc4e6ce2046fa5be3eefc0b9a4fa301b0e
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-11-12 21:59:24 -0300

    refactor: get all cart with coffe object

--- api/pkg/cart/models/getAll.go
+++ api/pkg/cart/models/getAll.go
@@ -3,17 +3,18 @@ package cart
 import (
 	"coffe-delivery-remix/api/entities"
 	"coffe-delivery-remix/api/services/db"
+	"encoding/json"
 	"log"
 )
 
-func GetAll() (cart []entities.CartProductSimple, err error) {
+func GetAll() (cart []entities.CartProduct, err error) {
 	connection, err := db.OpenConnection()
 	if err != nil {
 		return
 	}
 	defer connection.Close()
 
-	sql := "SELECT * FROM cart"
+	sql := `SELECT cart.id, cart.quantity, json_build_object('id', coffes.id, 'img', coffes.img, 'price', coffes.price, 'title', coffes.title, 'description', coffes.description, 'stok', coffes.stok, 'categories', coffes.categories) FROM cart JOIN coffes ON cart.productId = coffes.id`
 
 	rows, err := connection.Query(sql)
 	if err != nil {
@@ -21,13 +22,15 @@ func GetAll() (cart []entities.CartProductSimple, err error) {
 	}
 
 	for rows.Next() {
-		var productCart entities.CartProductSimple
+		var productCart entities.CartProduct
+		var jsonObject []byte
 
-		err = rows.Scan(&productCart.ID, &productCart.Quantity, &productCart.ProductId)
+		err = rows.Scan(&productCart.ID, &productCart.Quantity, &jsonObject)
 		if err != nil {
 			log.Printf("Error: %v\n", err.Error())
 			continue
 		}
+		json.Unmarshal([]byte(jsonObject), &productCart.Product)
 
 		cart = append(cart, productCart)
 	}
