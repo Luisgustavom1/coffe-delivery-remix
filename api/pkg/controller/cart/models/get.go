4735b6205e7cbcd1a81539fbf80934b290fe712c
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-11-13 13:14:13 -0300

    feat: get cart by product id

--- api/pkg/controller/cart/models/get.go
+++ api/pkg/controller/cart/models/get.go
@@ -6,7 +6,7 @@ import (
 	"coffe-delivery-remix/api/services/db"
 )
 
-func GetBy(id int64) (cart entities.ProductCart, err error) {
+func GetById(id int64) (cart entities.ProductCart, err error) {
 	connection, err := db.OpenConnection()
 	if err != nil {
 		return
@@ -17,7 +17,26 @@ func GetBy(id int64) (cart entities.ProductCart, err error) {
 
 	row := connection.QueryRow(sql, id)
 
-	var jsonProduct []byte 
+	var jsonProduct []byte
+	err = row.Scan(&cart.ID, &cart.Quantity, &jsonProduct)
+
+	err = serialize.Cart(jsonProduct, &cart)
+
+	return cart, err
+}
+
+func GetByProductId(id int64) (cart entities.ProductCart, err error) {
+	connection, err := db.OpenConnection()
+	if err != nil {
+		return
+	}
+	defer connection.Close()
+
+	sql := `SELECT cart.id, cart.quantity, json_build_object('id', coffes.id, 'img', coffes.img, 'price', coffes.price, 'title', coffes.title, 'description', coffes.description, 'stok', coffes.stok, 'categories', coffes.categories) FROM cart JOIN coffes ON cart.productId = coffes.id WHERE product.id=$1`
+
+	row := connection.QueryRow(sql, id)
+
+	var jsonProduct []byte
 	err = row.Scan(&cart.ID, &cart.Quantity, &jsonProduct)
 
 	err = serialize.Cart(jsonProduct, &cart)
