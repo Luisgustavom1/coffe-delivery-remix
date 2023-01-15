d2cd99c5f943e09200650838f626240e20d81b1e
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-01-15 19:37:31 -0300

    feat: delete cart when execute chekcout

--- api/pkg/controller/checkout/handlers/execute.go
+++ api/pkg/controller/checkout/handlers/execute.go
@@ -1,6 +1,8 @@
 package checkout
 
 import (
+	"coffe-delivery-remix/api/pkg/controller/cart/models"
+
 	"coffe-delivery-remix/api/models"
 	"coffe-delivery-remix/api/pkg/serialize"
 	"coffe-delivery-remix/api/services/email"
@@ -8,14 +10,37 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func Checkout(w http.ResponseWriter, request *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(request, "id"))
+	if err != nil {
+		log.Printf("Erro ao fazer o parse do query param id: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	rowsAffected, err := cart.DeleteBy(int64(id))
+	if err != nil {
+		log.Printf("Erro ao deletar carrinho: %v", id)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	if rowsAffected == 0 {
+		log.Printf("Carrinho não encontrado")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
 	var modelEmail models.EmailSimple
-	err := json.NewDecoder(request.Body).Decode(&modelEmail)
+	err = json.NewDecoder(request.Body).Decode(&modelEmail)
 
 	if err != nil {
-		log.Printf("Error ao decodificar o json: %v", err)
+		log.Printf("Erro ao decodificar o json: %v", err)
 		return
 	}
 
