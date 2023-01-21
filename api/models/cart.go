a565f61404d13d2ef747926743029c851dee5f6a
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-01-21 16:44:09 -0300

    feat: change db relations

--- api/models/cart.go
+++ api/models/cart.go
@@ -1,5 +1,7 @@
 package models
 
+// TODO
+// ALTERAR PARA ---> 1 para muitos com os produtos ---> 1 carrinho para muitos produtos
 type ProductCart struct {
 	ID       int64 `json:"id"`
 	Quantity int64 `json:"quantity"`
