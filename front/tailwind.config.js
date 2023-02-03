7a14c95d8e69d338c2577f58febee4f50b0b21e7
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-02-02 23:10:39 -0300

    feat: add button-active in utilities

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -5,21 +5,28 @@ module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: theme,
   plugins: [
-    plugin(({ addComponents }) => 
+    plugin(function({ addComponents, addUtilities }) {
       addComponents(Object.entries(theme.typography).reduce((accTypography, [key, values]) => {
-            const defaultStyle = values.DEFAULT;
-            delete values.DEFAULT;
+        const defaultStyle = values.DEFAULT;
+        delete values.DEFAULT;
 
-            return Object.entries(values).reduce((accVariants, [variants, styles]) => ({
-                ...accVariants, 
-                [`.typography-${key}-${variants}`]: {
-                    ...defaultStyle,
-                    ...styles,
-                }
-            }), {
-                ...accTypography
-            })
-        }, {}))
+        return Object.entries(values).reduce((accVariants, [variants, styles]) => ({
+            ...accVariants, 
+            [`.typography-${key}-${variants}`]: {
+                ...defaultStyle,
+                ...styles,
+            }
+        }), {
+            ...accTypography
+        })
+      }, {}))
+
+      addUtilities({
+        '.button-active': {
+          'box-shadow': 'inset 0px -1px 20px 2px rgba(128, 71, 248, 30%)'
+        }
+      })
+      }
     )
   ]
 };
