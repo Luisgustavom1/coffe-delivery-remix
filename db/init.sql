eaf2d876b7d05371b029c960e1dc0f0b8e99c079
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-04-21 15:59:36 -0300

    feat: create a function trigger to set the updated at

--- db/init.sql
+++ db/init.sql
@@ -4,6 +4,14 @@ CREATE DATABASE coffee_delivery_remix_db;
 ALTER user user_coffee WITH ENCRYPTED PASSWORD '1459';
 GRANT ALL privileges ON DATABASE coffee_delivery_remix_db TO user_coffee;
 \c coffee_delivery_remix_db;
+
+CREATE OR REPLACE FUNCTION trigger_timestamps() RETURNS TRIGGER AS $$
+  BEGIN
+    NEW.updated_at = NOW();
+    return NEW;
+  END;
+$$ LANGUAGE plpgsql;
+
 CREATE TABLE product (
   id serial primary key, 
   img varchar, 
@@ -12,13 +20,40 @@ CREATE TABLE product (
   description text, 
   stok integer, 
   categories varchar,
-  type varchar
+  type varchar,
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
-GRANT ALL privileges ON ALL tables IN SCHEMA public TO user_coffee;
 
 CREATE TABLE cart (
+  id serial primary key
+);
+
+CREATE TABLE cart_product (
   id serial PRIMARY KEY,
   product_id INT NOT NULL,
+  cart_id INT NOT NULL,
   quantity integer,
-  CONSTRAINT fk_product FOREIGN KEY(product_id) REFERENCES product(id)
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  CONSTRAINT fk_cart FOREIGN KEY(cart_id) REFERENCES cart(id) ON DELETE CASCADE,
+  CONSTRAINT fk_product FOREIGN KEY(product_id) REFERENCES product(id) ON DELETE CASCADE
 );
+
+-- Create timestamps TRIGGER to all tables that have the column 
+DO $$
+DECLARE 
+  t text;
+BEGIN
+  FOR t IN 
+    SELECT table_name FROM information_schema.columns WHERE column_name = 'updated_at'
+  LOOP
+    EXECUTE format('
+      CREATE TRIGGER timestamps 
+        BEFORE UPDATE ON %I
+        FOR EACH ROW EXECUTE FUNCTION trigger_timestamps();', t);
+  END LOOP; 
+END;
+$$ LANGUAGE plpgsql;
+
+GRANT ALL privileges ON ALL tables IN SCHEMA public TO user_coffee;
