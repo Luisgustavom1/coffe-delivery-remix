4b1d61150fb8a8e46cc22ed8f95afd46ba732eae
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-04-23 21:40:10 -0300

    refactor: UpdateCartProduct

--- api/usecase/cart/update.go
+++ api/usecase/cart/update.go
@@ -11,24 +11,32 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func (c *CartUseCase) UpdateBy(w http.ResponseWriter, request *http.Request) {
+type Input struct {
+	Quantity int64 `json:"quantity"`
+}
+
+func (c *CartUseCase) UpdateCartProduct(w http.ResponseWriter, request *http.Request) {
 	id, err := strconv.Atoi(chi.URLParam(request, "id"))
+	productId, err := strconv.Atoi(chi.URLParam(request, "productId"))
 	if err != nil {
-		log.Printf("Erro ao fazer o parser do query param id: %v\n", err)
+		log.Printf("Erro ao fazer o parse dos query params: %v", err)
 		http_error.HttpError(w, 500)
 		return
 	}
 
-	var cartUpdate entities.CartProductSimple
+	var input Input
 
-	err = json.NewDecoder(request.Body).Decode(&cartUpdate)
+	err = json.NewDecoder(request.Body).Decode(&input)
 	if err != nil {
 		log.Printf("Erro ao fazer o decode do json: %v\n", err)
 		http_error.HttpError(w, 500)
 		return
 	}
 
-	rowsAffected, err := c.cartRepository.UpdateProductByCartId(int64(id), cartUpdate)
+	rowsAffected, err := c.cartRepository.UpdateCartProductByCartId(int64(id), entities.CartProductSimple{
+		ProductId: int64(productId),
+		Quantity:  input.Quantity,
+	})
 	if err != nil {
 		log.Printf("Erro ao atualizar registro: %v", id)
 		http_error.HttpError(w, 500)
